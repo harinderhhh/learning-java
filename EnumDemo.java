8e25baa9b1ff484209921866251ca9234ce3fe4c
harinderhhh <harinderhhh@gmail.com>
2018-03-28 09:24:53 +0530

    Uploading first set of files from local laptop

---
+++ EnumDemo.java
@@ -0,0 +1,18 @@
+package org.java.enumdemo;
+
+enum Beer
+{
+	KF, KO, RC, FO;
+}
+
+public class EnumDemo {
+
+	public static void main(String[] args) {
+		Beer[] b = Beer.values();
+		for (Beer b1 : b)
+		{
+			System.out.println(b1);
+		}
+	}
+
+}
---
+++ ThreadGroupDemo.java
@@ -0,0 +1,18 @@
+package com.multithreadingenhancements;
+
+import java.util.concurrent.locks.Lock;
+
+public class ThreadGroupDemo {
+
+	public static void main(String[] args) {
+		//Lock
+		ThreadGroup systemGroup = Thread.currentThread().getThreadGroup().getParent();
+		Thread[] t = new Thread[systemGroup.activeCount()];
+		systemGroup.enumerate(t);
+		for (Thread i : t)
+		{
+			System.out.println(i.getName() + "---" + i.isDaemon());
+		}
+	}
+
+}
